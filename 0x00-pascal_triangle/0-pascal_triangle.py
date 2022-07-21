1b4943926dea440a0dd58ff62805c77866f0ed87
jeremy302 <olajeremy123@gmail.com>
2022-07-21 12:52:09 +0100

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+def pascal_triangle(n):
+    ''' generates pascals triangle '''
+    triangle = []
+    v = [1]
+    for i in range(1, n):
+        triangle.append(v)
+        v = [v[0] if not j else v[-1] if j == i else v[j-1] + v[j]
+             for j in range(i+1)]
+    if n > 0:
+        triangle.append(v)
+    return triangle
