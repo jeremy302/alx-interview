f3bdd9dde7fe7babcf53e287e03b6713451c67a4
jeremy302 <olajeremy123@gmail.com>
2023-04-06 21:35:13 +0100

    patch

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,9 +5,10 @@ from functools import reduce
 
 def minOperations(n):
     ''' gets minumim number of operations '''
-    factors = (set(
+    factors = (sorted(
         reduce(list.__add__, ([i, n//i] for i in range(
-            1, int(n**0.5) + 1) if n % i == 0))) if n >= 0 else [])
+            1, int(n**0.5) + 1) if n % i == 0))) if n >= 0 else [])[1:]
+
     ops = 0
     n = 1
     for f in factors:
