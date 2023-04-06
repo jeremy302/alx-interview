72477e0b52212aa0ce9286c45ec48474ebcc98bd
jeremy302 <olajeremy123@gmail.com>
2023-04-06 21:27:23 +0100

    optimized factors finding

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 ''' minimum operations module '''
+from functools import reduce
 
 
 def minOperations(n):
     ''' gets minumim number of operations '''
-    factors = [v for v in range(2, n + 1) if n % v == 0]
+    factors = (set(
+        reduce(list.__add__, ([i, n//i] for i in range(
+            1, int(n**0.5) + 1) if n % i == 0))) if n >= 0 else [])
     ops = 0
     n = 1
     for f in factors:
