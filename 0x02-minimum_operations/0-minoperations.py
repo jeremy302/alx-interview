21c818e2c464f704168665934a97bf91f7b7c262
jeremy302 <olajeremy123@gmail.com>
2023-04-06 21:46:15 +0100

    fixed n=0 bug

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,7 +7,7 @@ def minOperations(n):
     ''' gets minumim number of operations '''
     factors = sorted(set(
         reduce(list.__add__, ([i, n//i] for i in range(
-            1, int(n**0.5) + 1) if n % i == 0))) if n >= 0 else [])[1:]
+            1, int(n**0.5) + 1) if n % i == 0))) if n > 0 else [])[1:]
 
     ops = 0
     n = 1
