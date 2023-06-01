f3a360a2064bde07ac66a4b54ec670c156ab0d24
jeremy302 <olajeremy123@gmail.com>
2023-06-01 18:06:07 +0100

    patch

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,15 +2,23 @@
 ''' prime game module '''
 
 
+def rwh_primes(n):
+    '''
+    adapted from:
+    https://stackoverflow.com/questions/2068372
+    /fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    '''
+    sieve = [True] * n
+    for i in range(3, int(n**0.5) + 1, 2):
+        if sieve[i]:
+            sieve[i * i::2 * i] = [False] * int((n - i * i - 1) / (2 * i) + 1)
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]
+
+
 def playMatch(n):
     ''' plays a single round '''
-    nums = list(range(2, n+1))
-    winner = 1
-    while nums:
-        pick = nums[0]
-        nums = [v for v in nums if v % pick]
-        winner = 1 - winner
-    return winner
+    primes = rwh_primes(n + 1)
+    return 1 - (len(primes) % 2) if n > 1 else 1
 
 
 def isWinner(x, nums):
